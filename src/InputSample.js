792c92f87f29fb2b143dc2f28eeed2ec01ebc881
rnthd2 <rnthd2@naver.com>
2021-04-09 13:27:17 +0900

    input 상태 관리

---
+++ src/InputSample.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+function InputSample(){
+    const [text, setText] = useState('');
+    //e : event 객체
+    const onChange = (e) => {
+        setText(e.target.value);
+    }
+    const onReset = () => {
+        setText('');
+    };
+    return(
+        <div>
+            <input onChange={onChange} value={text}/>
+            <button onClick={onReset}>초기화</button> 
+            <div>
+                <b>값: </b>
+                {text}
+            </div>
+        </div>
+    );
+}
+export default InputSample;

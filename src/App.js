558e9c8d04406912c851720331952b7b9990ed89
rnthd2 <rnthd2@naver.com>
2021-04-14 13:34:11 +0900

    useCallback을 이용한 랜더링 시 불필요한 함수 호출 막기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useMemo} from 'react';
+import React, {useRef, useState, useMemo, useCallback} from 'react';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
 // import InputSample from './InputSample';
@@ -16,13 +16,17 @@ function App() {
     email:'',
   });
   const {username, email} = inputs;
-  const onChange = e =>{
+
+  //useCallback 함수를 사용하기 위해 아래와 같이 이벤트를 감싼다
+  //useCallback 함수는 depth에 들어가는 inputs가 바뀔 때 마다 실행된다
+  //useCallback 함수없이는 컴포넌트가 매번 리랜더링 될때마다 새로운 함수를 만든다
+  const onChange = useCallback(e =>{
     const{name, value} = e.target;
     setInputs({
       ...inputs,
       [name]: value
     });
-  }
+  }, [inputs]);
 
   const [users,setUsers] = useState([
     {
@@ -47,8 +51,9 @@ function App() {
   //굳이 그럴 필요가 없을 때, useRef로 해도됨~
   const nextId = useRef(4);
 
-  const onCreate = () => {
-
+  //useCallback에 depth 배열에 참조하고 있는 값을 넣지 않으면
+  //컴포넌트가 만들어질때 상태를 참조하기 때문에 최신 상태를 참조할 수 없다
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       ...inputs
@@ -68,21 +73,21 @@ function App() {
     }); 
     // console.log(nextId.current);  //4
     nextId.current += 1;  //컴포넌트가 리랜더링 되지 않는다
-  };
+  }, [inputs, users]);
 
-  const onRemove = id => {
+  const onRemove = useCallback(id => {
     setUsers(users.filter(user => user.id !== id));
-  }
+  }, [users]);
 
   //배열에 있는 특정 item만 업데이트 할 때에도 map을 사용한다
   //id값을 비교해 spread로 기존값을 복사하고 active값만 토글한다
-  const onToggle = id => {
+  const onToggle = useCallback(id => {
     setUsers(users.map(
       user => user.id === id
        ? {...user, active: !user.active}
        : user
     ))
-  }
+  }, [users]);
   
   //해당 함수의 기능을 할 때만 해당 함수를 사용하고 싶을 때, useMemo를 사용한다
   const count = useMemo(() => countActiveUsers(users), [users]);
